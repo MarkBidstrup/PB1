e5b3f97bba910891fb172e2a1c832f217a0cf492
Mark <s215782@student.dtu.dk>
2022-10-17 21:37:47 +0200

    testing webhook

--- PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/ui/ProfilePatientScreen.kt
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/ui/ProfilePatientScreen.kt
@@ -27,7 +27,6 @@ import com.example.pb1_probe_application.ui.theme.Typography
 @Composable
 fun ProfilePatienScreen() {
     PatientInfoList(patientInfoList = Datasource().loadProfilePatientInfo())
-//    ScaffoldDemo()
 }
 
 @Composable
