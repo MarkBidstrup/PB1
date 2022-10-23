f4ec65795e467002c039c6b4065e8270ee7ca9d0
Mark <s215782@student.dtu.dk>
2022-10-23 10:47:07 +0200

    changed lineheight in trial posts

--- PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/MainActivity.kt
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 //                    Greeting("Android")
-                    NotificationsScreen()
+                    MyTrialsResearcher()
                 }
             }
         }
--- PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/ui/MyTrialsResearcher.kt
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/ui/MyTrialsResearcher.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pb1_probe_application.R
 import com.example.pb1_probe_application.model.TrialState
@@ -124,8 +125,8 @@ fun ResearcherTrialPost(trialInfo: TrialState, modifier: Modifier = Modifier) {
                     text = trialInfo.trialName,
                     style = MaterialTheme.typography.body1,
                     fontWeight = FontWeight.Bold,
-                    color = androidx.compose.ui.graphics.Color.Black
-
+                    color = androidx.compose.ui.graphics.Color.Black,
+                    lineHeight = 20.sp
                 )
                 Text(
                     text = stringResource(R.string.tilmeldingsfrist) + " "+ trialInfo.registrationDeadline,
