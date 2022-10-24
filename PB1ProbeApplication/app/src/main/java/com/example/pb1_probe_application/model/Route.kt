1500398428947d7eafaed97de2377df9339652f2
Fathionsons <gathefathe29@gmail.com>
2022-10-24 22:51:15 +0200

    navigating ++

---
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/model/Route.kt
@@ -0,0 +1,7 @@
+package com.example.pb1_probe_application.model
+// routes ( bottombar items routes not included )
+sealed class Route(val route: String) {
+
+    object Setting: Route("Setting")
+    object EditProfile: Route("EditProfile")
+}
