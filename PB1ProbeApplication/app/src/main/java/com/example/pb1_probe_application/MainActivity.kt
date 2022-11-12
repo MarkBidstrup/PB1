3b8a1b692b42f7c6d16c6b823a3fd44e1b9dd595
Mark <s215782@student.dtu.dk>
2022-11-12 09:30:57 +0100

    created manage trial screen

--- PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/MainActivity.kt
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
                 ) {
 
 //                       MainHome()
-                    EditTrialScreen()
+                    ManageTrialScreen()
                 }
             }
         }
---
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/ui/ManageTrialScreen.kt
@@ -0,0 +1,74 @@
+package com.example.pb1_probe_application.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.rememberNavController
+import com.example.pb1_probe_application.R
+import com.example.pb1_probe_application.model.Role
+import com.example.pb1_probe_application.model.Route
+import com.example.pb1_probe_application.ui.theme.TextColorRed
+import com.example.pb1_probe_application.ui.theme.Typography
+
+@Composable
+fun ManageTrialScreen() {
+
+    Scaffold(
+
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                title = { Text(stringResource(R.string.håndterStudie), style = Typography.h1) },
+                backgroundColor = MaterialTheme.colors.onPrimary)
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End) {
+                IconButton(onClick = {
+                    //TODO onClick
+                }) {
+                    Icon(
+                        Icons.Default.ArrowBack,
+                        contentDescription = "back arrow"
+                    )
+                }
+            }
+        },
+        content = {
+            Column() {
+                Text(
+                    text = stringResource(id = R.string.redigerStudie),
+                    style = Typography.body1,
+                    modifier = Modifier.padding(start = 17.dp, end = 17.dp, bottom = 10.dp, top = 10.dp)
+                )
+                Divider(
+                    thickness = 1.dp,
+                    color = androidx.compose.ui.graphics.Color.LightGray,
+                    modifier = Modifier.padding(start = 17.dp, end = 17.dp)
+                )
+                Text(
+                    text = stringResource(R.string.seDeltagerliste),
+                    style = Typography.body1,
+                    modifier = Modifier.padding(start = 17.dp, end = 17.dp, bottom = 10.dp, top = 10.dp)
+                )
+                Divider(
+                    thickness = 1.dp,
+                    color = androidx.compose.ui.graphics.Color.LightGray,
+                    modifier = Modifier.padding(start = 17.dp, end = 17.dp)
+                )
+                Text(
+                    text = stringResource(R.string.sletStudie),
+                    style = Typography.body1,
+                    color = TextColorRed,
+                    modifier = Modifier.padding(start = 17.dp, end = 17.dp, bottom = 10.dp, top = 10.dp)
+                )
+            }
+        }
+    )
+}
