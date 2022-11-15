c2c67cd961102651d9c4366e615c59aefe5a21c8
Xiao Chen <xchen2305@gmail.com>
2022-11-15 21:38:37 +0100

    Simplified data structure - use maps for trial locations instead of nested subcollections. Checked that add and update methods work

--- PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/model/service/TrialServiceImpl.kt
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/model/service/TrialServiceImpl.kt
@@ -1,8 +1,6 @@
 package com.example.pb1_probe_application.model.service
 
-import androidx.compose.animation.core.snap
 import com.example.pb1_probe_application.model.Trial
-import com.example.pb1_probe_application.model.TrialLocation
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -23,14 +21,7 @@ class TrialServiceImpl : TrialService {
     override suspend fun getAllTrials(): List<Trial>? {
         val list: MutableList<Trial> = ArrayList()
         val snapshot = currentCollection().get().await()
-        snapshot.forEach { t ->
-            val trial = t.toObject<Trial>()
-            val locs = currentCollection().document(t.id).collection("locations").get().await()
-            val locations = locs.documents.map { it.toObject<TrialLocation>() }
-            if (locations[0] != null)
-                trial.locations = locations as List<TrialLocation>
-            list.add(trial)
-        }
+        snapshot.forEach { t -> list.add(t.toObject()) }
         return if(list.size == 0)
             null
         else
@@ -38,12 +29,7 @@ class TrialServiceImpl : TrialService {
     }
 
     override suspend fun getTrial(trialId: String): Trial? {
-        val trial = currentCollection().document(trialId).get().await().toObject<Trial>()
-        val snapshot = currentCollection().document(trialId).collection("locations").get().await()
-        val locations = snapshot.documents.map { it.toObject<TrialLocation>() }
-        if (trial != null && locations[0] != null)
-            trial.locations = locations as List<TrialLocation>
-        return trial
+        return currentCollection().document(trialId).get().await().toObject<Trial>()
     }
 
     override suspend fun getFilteredTrials(
@@ -54,13 +40,12 @@ class TrialServiceImpl : TrialService {
         TODO("Not yet implemented")
     }
 
-    // TODO - make sure that nested objects (locations) get added correctly
     override suspend fun addNew(trial: Trial) {
-        currentCollection().add(trial)
+        currentCollection().add(trial).await()
     }
 
     override suspend fun update(trial: Trial) {
-        currentCollection().document(trial.trialID).set(trial)
+        currentCollection().document(trial.trialID).set(trial).await()
     }
 
     override suspend fun delete(trialId: String) {
