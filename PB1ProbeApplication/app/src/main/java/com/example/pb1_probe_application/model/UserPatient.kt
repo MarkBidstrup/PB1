acda3c7a9bf508830e6bc460d819618ae1cf77fd
Thor Jensen <thor@iwanoff.dk>
2022-11-21 12:46:18 +0100

    Updated UserPatient.kt data types

--- PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/model/UserPatient.kt
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/model/UserPatient.kt
@@ -1,12 +1,15 @@
 package com.example.pb1_probe_application.model
 
+import com.google.firebase.firestore.DocumentId
+
 data class UserPatient (
-    var navn: String,
-    var efternavn: String,
-    var koen: String,
-    var alder: String,
-    var vaegt: String,
-    var diagnose: String,
+    @DocumentId val userID: String,
+    var name: String,
+    var lastName: String,
+    var gender: String,
+    var age: Int,
+    var weight: Float,
+    var diagnosis: String,
     var email: String,
-    var telefon: String,
+    var phone: String,
 )
