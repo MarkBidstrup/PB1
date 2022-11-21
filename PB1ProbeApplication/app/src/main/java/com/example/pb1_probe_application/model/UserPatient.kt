44a25059547306cf704d85f463050105cbffa903
Thor Jensen <thor@iwanoff.dk>
2022-11-21 12:47:50 +0100

    Updated UserPatient.kt data types again

--- PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/model/UserPatient.kt
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/model/UserPatient.kt
@@ -3,13 +3,13 @@ package com.example.pb1_probe_application.model
 import com.google.firebase.firestore.DocumentId
 
 data class UserPatient (
-    @DocumentId val userID: String,
-    var name: String,
-    var lastName: String,
-    var gender: String,
-    var age: Int,
-    var weight: Float,
-    var diagnosis: String,
-    var email: String,
-    var phone: String,
+    @DocumentId val userID: String = "",
+    var name: String = "",
+    var lastName: String = "",
+    var gender: String = "",
+    var age: Int = 0,
+    var weight: Double = 0.0,
+    var diagnosis: String = "",
+    var email: String = "",
+    var phone: String = "",
 )
