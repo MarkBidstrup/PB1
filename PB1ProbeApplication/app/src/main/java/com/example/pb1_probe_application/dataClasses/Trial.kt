fcac4fc4834b1d0fee834a79966ec0af15fbe7db
Mark <s215782@student.dtu.dk>
2023-01-04 12:02:43 +0100

    list of diagnoses, added "kommuner", changed location to string instead of list.

--- PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/dataClasses/Trial.kt
+++ PB1ProbeApplication/app/src/main/java/com/example/pb1_probe_application/dataClasses/Trial.kt
@@ -37,7 +37,7 @@ data class dbRegistrations(
 enum class trialAttributes {
     title, purpose, briefDescription, numParticipants, registrationDeadline, inclusionCriteria,
     exclusionCriteria, transportComp, compensation, lostSalaryComp, trialDuration, numVisits, startDate, endDate,
-    forsoegsBeskrivelse, deltagerInformation, locations, interventions, diagnoses
+    forsoegsBeskrivelse, deltagerInformation, locations, kommuner, interventions, diagnoses
 }
 
 fun loadCreateTrialList(): List<CreateTrialField> {
